9501c74352a1b476c4dc1a5e0950e45dd97d1cbc
Bruno-Kazeker <brunohkazeker@gmail.com>
2025-07-22 12:25:03 -0300

    Add files via upload

---
+++ sala04/atividade1/script.js
@@ -0,0 +1,6 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNunber
+    var numero2 = document.getElementById("n2").valueAsNunber
+    document.getElementById('resposta').textContent  = numero1 + numero2
+    var resposta
+}
